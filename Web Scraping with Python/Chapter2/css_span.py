cdb643f19596d6f4586d39cba68074952ddb5d9a
Jimmy Xiang <xxg1413@gmail.com>
2016-03-27 19:00:01 -0600

    add css_span.py

---
+++ Web Scraping with Python/Chapter2/css_span.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+from urllib.request import  urlopen
+from bs4 import BeautifulSoup
+
+#html = urlopen("http://www.baidu.com")
+html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+
+data = BeautifulSoup(html.read(), "html.parser")
+
+name_list = data.findAll("span", {"class":"green"})
+
+for name in name_list:
+    print(name.get_text())
