4bd5f351bc5ad0d92bdc123b47ae7f739ae55f70
Jimmy Xiang <xxg1413@gmail.com>
2016-04-06 22:14:37 +0800

    update chapater2

--- Web Scraping with Python/Chapter2/css_span.py
+++ Web Scraping with Python/Chapter2/css_span.py
@@ -8,7 +8,13 @@ html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 
 data = BeautifulSoup(html.read(), "html.parser")
 
+'''
 name_list = data.findAll("span", {"class":"green"})
 
 for name in name_list:
     print(name.get_text())
+
+'''
+
+prince_list = data.findAll(text="the prince")
+print(len(prince_list))
