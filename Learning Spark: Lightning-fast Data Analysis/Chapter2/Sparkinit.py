4658a7b5fc8389a0a94d80c8a7824884bf39868b
Jimmy Xiang <xxg1413@gmail.com>
2016-05-10 20:47:53 +0800

    update

--- Learning Spark: Lightning-fast Data Analysis/Chapter2/Sparkinit.py
+++
@@ -1,6 +0,0 @@
-from pyspark import  SparkConf, SparkContext
-
-conf = SparkConf().setMaster("local").setAppName("my app")
-
-sc = SparkContext(conf= conf)
-
--- Learning Spark: Lightning-fast Data Analysis/README.md
+++
@@ -1,7 +0,0 @@
-##Spark 快速大数据分析
-
-
-
-
-##Learning Spark: Lightning-fast Data Analysis
-
