1bfe55825aa02b0795761adfedd65df2de15d637
Jimmy Xiang <xxg1413@gmail.com>
2016-05-27 11:12:00 -0600

    add analyzen.py

---
+++ Building Machine Learning Systems/Chapter1/Project1/analyzen.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import os
+import  scipy as sp
+import  matplotlib.pyplot as plt
+
+data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),"data")
+
+#print(data_dir)
+
+data_path = os.path.join(data_dir, "web_traffic.tsv")
+
+if os.path.exists(data_path):
+    data = sp.genfromtxt(data_path, delimiter="\t")#必须是Tab
+    #print(data[:2])
+else:
+    print("data not exist!")
+    exit(0)
+
+#print(data.shape)
+
+#把数据(x,y)分为两个向量来处理
+
+x = data[:,0]
+y = data[:,1]
+
+#因为有值为nan的情况，
+#print sp.sum(sp.isnan(y))
+#print len(x), len(y)
+
+days = len(y)/24 + 1
+
+#数据清洗，删除为nan的项
+x = x[~sp.isnan(y)]
+y = y[~sp.isnan(y)]
+
+
+#print len(x), len(y)
+#print sp.sum(sp.isnan(y))
+
+#计算误差
+def error(f, x, y):
+    return sp.sum((f(x) - y)**2 )
+
+
+fp1, residuals, rank, sv, rcond = sp.polyfit(x,y,1,full=True)
+
+f1 = sp.poly1d(fp1)
+f2 = sp.poly1d(sp.polyfit(x, y, 2))
+f3 = sp.poly1d(sp.polyfit(x,y,3))
+f10 = sp.poly1d(sp.polyfit(x,y,10))
+f100 = sp.poly1d(sp.polyfit(x,y,100))
+
+
+
+fx = sp.linspace(0,x[-1],1000)
+plt.plot(fx, f1(fx), linewidth=4)
+plt.legend(["d=%i" % f1.order], loc="upper left")
+
+
+print(error(f1,x,y))
+
+
+
+##曲线拟合 d=2
+
+
+
+
+#散点图
+plt.scatter(x,y)
+plt.title("Web traffic over the last month")
+plt.xlabel("Time")
+plt.ylabel("Hita/hour")
+
+plt.xticks([w*7*24 for w in range(10)], ['week %i' %w for w in range(10)])
+
+plt.autoscale(tight=True)
+plt.grid()
+plt.show()
+
+
+
+
